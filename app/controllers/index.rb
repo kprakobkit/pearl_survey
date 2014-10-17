965b72a2c9debb6235bc39183af73e15f43752c3
Peter Prakobkit <kprakobkit@gmail.com>
2014-10-16 17:37:39 -0700

    able to save survey title

---
+++ app/controllers/index.rb
@@ -0,0 +1,29 @@
+# get main page '/surveys'
+# post '/surveys'
+get '/' do
+  redirect '/surveys'
+end
+
+get '/surveys' do
+  @surveys = Survey.all
+
+  erb :index
+end
+
+get '/surveys/create' do
+  erb :survey_create
+end
+
+post '/surveys' do
+  content_type :json
+  survey = Survey.create(title: params[:title])
+  {title: survey.title}.to_json
+end
+
+# post '/surveys/add_question' do
+#   survey = Survey.last
+#   survey << Question.create(description: params[:description])
+
+# end
+
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#create_survey_form').on('submit', function(event){
+  event.preventDefault();
+  console.log($(this).serialize());
+  $(this).css('display', 'none');
+  var ajaxRequest = $.ajax({
+    url: '/surveys',
+    type: 'POST',
+    data: $(this).serialize()
+  }).done(function(data){
+    title = data.title;
+    $('.container').prepend($('<h1>Title: ' + data.title + '</h1>'));
+  });
+})
 });
